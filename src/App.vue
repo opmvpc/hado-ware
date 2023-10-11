a47c24637cbe39d2cd35d37b31c6aa9a5b4fb0b5
opmvpc <thibsix@outlook.be>
2023-10-11 17:11:23 +0200

    boucle

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,59 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
 
-<template></template>
+let canvas: HTMLCanvasElement;
+let ctx: CanvasRenderingContext2D;
+
+let requestId: number;
+let startTimeStamp: number;
+
+const doThings = () => {
+  const leCanvas = document.querySelector("canvas");
+  if (leCanvas === null) {
+    return;
+  }
+
+  canvas = leCanvas;
+
+  canvas.height = document.body.clientHeight;
+  canvas.width = document.body.clientWidth;
+
+  const leCtx = canvas.getContext("2d");
+  if (leCtx === null) {
+    return;
+  }
+
+  ctx = leCtx;
+
+  ctx.fillStyle = "green";
+  ctx.fillRect(10, 10, 150, 100);
+};
+
+const step = (timeStamp: number) => {
+  const delta = timeStamp - startTimeStamp / 1000;
+  window.requestAnimationFrame(step);
+};
+
+const handleResize = () => {
+  if (canvas === null) {
+  }
+  canvas.height = document.body.clientHeight;
+  canvas.width = document.body.clientWidth;
+};
+
+onMounted(() => {
+  doThings();
+
+  window.addEventListener("resize", handleResize);
+
+  window.requestAnimationFrame(step);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", handleResize);
+});
+</script>
+
+<template>
+  <canvas ref="canvas"></canvas>
+</template>
